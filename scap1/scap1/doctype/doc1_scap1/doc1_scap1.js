2ea9e2e96c0ad3751d0021bf2a18cd11f6ce8716
nishchay2698 <nishchay.garala@gmail.com>
2021-02-05 10:17:08 +0000

    first doc and report created

---
+++ scap1/scap1/doctype/doc1_scap1/doc1_scap1.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, sprics and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('doc1 scap1', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ scap1/scap1/report/doc1_rep1/doc1_rep1.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, sprics and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["doc1 rep1"] = {
+	"filters": [
+
+	]
+};
