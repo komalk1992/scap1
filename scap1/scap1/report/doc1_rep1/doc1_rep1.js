e2d0babb0c8ce961c4ad704054e94fecd7b55299
nishchay2698 <nishchay.garala@gmail.com>
2021-02-05 11:31:14 +0000

    App Initialized 2

--- scap1/scap1/report/doc1_rep1/doc1_rep1.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2016, sprics and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.query_reports["doc1 rep1"] = {
-	"filters": [
-
-	]
-};
--- scap1/scap1/report/scap1_rep2/scap1_rep2.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2016, sprics and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.query_reports["scap1 rep2"] = {
-	"filters": [
-
-	]
-};
