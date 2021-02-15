a9c0727fc88949e0fd032c2419c8fb9330d191ac
nishchay2698 <nishchay.garala@gmail.com>
2021-02-15 09:04:15 +0000

    data

--- scap1/scap1/report/prosales/prosales.py
+++ scap1/scap1/report/prosales/prosales.py
@@ -1,9 +1,3 @@
-# Copyright (c) 2013, sprics and contributors
-# For license information, please see license.txt
-
-# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
-# For license information, please see license.txt
-
 from __future__ import unicode_literals
 from frappe import _
 import frappe
@@ -15,14 +9,14 @@ def execute(filters=None):
 def get_columns():
 	columns = [
 		{
-			"fieldname":"item",
 			"label": _("Item"),
+			"fieldname": "item_code",
 			"fieldtype": "Link",
 			"options": "Item",
-			"width": 120
+			"width": 150
 		},
 		{
-			"label": _("Total"),
+			"label": _("Amount"),
 			"fieldname": "amount",
 			"fieldtype": "Float",
 			"width": 120
@@ -31,18 +25,25 @@ def get_columns():
 	return columns
 
 def get_data(filters):
-	return frappe.db.sql("""
-		SELECT
-				`tabSales Order Item`.`item_code`,
-				`tabSales Order`.amount
-		FROM `tabSales Order Item`,`tabSales Order`
-		where `tabSales Order Item`.`parent` = `tabSales Order`.`name`
-		AND `tabSales Order Item`.`item_code` = %(item)s
-		{conditions}
-		GROUP BY `tabSales Order Item`.`item_code` """.format(conditions=get_conditions(filters)), filters, as_dict=1)
-		def get_conditions(filters) :
+        datasales =  frappe.db.sql("""
+                SELECT
+                        `tabSales Order Item`.item_code,
+                        SUM(`tabSales Order Item`.amount)
+                FROM
+                        `tabSales Order Item`,`tabSales Order`
+                WHERE
+                        `tabSales Order Item`.`parent`=`tabSales Order`.`name`
+			 AND `tabSales Order`.transaction_date BETWEEN %(from_date)s AND %(to_date)s
+                        {conditions}
+                GROUP BY
+                        `tabSales Order Item`.item_code """.format(conditions=get_conditions(filters)), filters,as_list=1)
+
+        return datasales
+
+def get_conditions(filters) :
 	conditions = []
 
-	if filters.get("item"):
-		conditions.append(" and `tabSales Order Item`.`item_code`=%(item)s")
+	if filters.get("item_code"):
+		conditions.append(" and `tabSales Order Item`.item_code=%(item_code)s")
+
 	return " ".join(conditions) if conditions else ""
---
+++ scap1/scap1/report/sa/sa.py
@@ -0,0 +1,49 @@
+from __future__ import unicode_literals
+from frappe import _
+import frappe
+
+def execute(filters=None):
+        columns, data = get_columns(), get_data(filters)
+        return columns, data
+
+def get_columns():
+        columns = [
+                {
+                        "label": _("Item"),
+                        "fieldname": "item_code",
+                        "fieldtype": "Link",
+                        "options": "Item",
+                        "width": 150
+                },
+                {
+                        "label": _("Amount"),
+                        "fieldname": "amount",
+                        "fieldtype": "Currency",
+                        "width": 120
+                }
+        ]
+        return columns
+
+def get_data(filters):
+        datasales =  frappe.db.sql("""
+                SELECT
+                        `tabSales Order Item`.item_code,
+                        `tabSales Order Item`.amount
+                FROM
+                        `tabSales Order Item`,`tabSales Order`
+                WHERE
+                        `tabSales Order Item`.`parent`=`tabSales Order`.`name`
+                        {conditions}
+                GROUP BY
+                        `tabSales Order Item`.item_code """.format(conditions=get_conditions(filters)), filters,
+as_list=1)
+
+        return datasales
+
+def get_conditions(filters) :
+        conditions = []
+
+        if filters.get("item_code"):
+                conditions.append(" and `tabSales Order Item`.item_code=%(item_code)s")
+
+        return " ".join(conditions) if conditions else ""
