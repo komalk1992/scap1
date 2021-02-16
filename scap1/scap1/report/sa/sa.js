5c3c4ef2de3a5c6c637467160e63cfd8881229cc
nishchay2698 <67817531+nishchay2698@users.noreply.github.com>
2021-02-16 11:05:42 +0530

    Update sa.js
    week

--- scap1/scap1/report/sa/sa.js
+++ scap1/scap1/report/sa/sa.js
@@ -26,7 +26,7 @@ frappe.query_reports["SA"] = {
 			label: __("Range"),
 			fieldtype: "Select",
 			options: [
-				{ "value": "Weekly", "label": __("Weekly") },
+				{ "value": "Weekly", "label": __("Week") },
 				{ "value": "Monthly", "label": __("Monthly") },
 				{ "value": "Quarterly", "label": __("Quarterly") },
 				{ "value": "Yearly", "label": __("Yearly") }
