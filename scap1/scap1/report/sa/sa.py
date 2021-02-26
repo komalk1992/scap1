0073745a75728bb9dc4d4a905fa4103e2c6eb1d0
nishchay2698 <67817531+nishchay2698@users.noreply.github.com>
2021-02-26 15:59:30 +0530

    Update sa.py

--- scap1/scap1/report/sa/sa.py
+++ scap1/scap1/report/sa/sa.py
@@ -27,7 +27,7 @@ def get_columns():
         ]
         return columns
 
-def get_data(filters.range === 'Weekly'):
+def get_data():
         datasales =  frappe.db.sql("""
                 SELECT
                         `tabSales Order Item`.item_code,
