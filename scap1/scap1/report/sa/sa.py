6b8548246849b0ae74a939dbd1231d8d55afced1
nishchay2698 <67817531+nishchay2698@users.noreply.github.com>
2021-02-26 12:13:58 +0530

    Update sa.py

--- scap1/scap1/report/sa/sa.py
+++ scap1/scap1/report/sa/sa.py
@@ -9,44 +9,38 @@ def execute(filters=None):
         columns, data = get_columns(), get_data(filters)
         return columns, data
 
-def get_conditions(filters) :
-        conditions = []
-        if filters.range == "Week":
-                columns, data = get_columns(), get_data(filters)
-                return columns, data
-                def get_columns():
-                        columns = [
-                                {
-                                        "label": _("Item"),
-                                        "fieldname": "item_code",
-                                        "fieldtype": "Link",
-                                        "options": "Item",
-                                        "width": 150
-                                },
-                                {
-                                        "label": _("Amount"),
-                                        "fieldname": "amount",
-                                        "fieldtype": "Float",
-                                        "width": 120
-                                }
-                        ]
-                        return columns
-        return " ".join(conditions) if conditions else ""
+def get_columns():
+        columns = [
+                {
+                        "label": _("Item"),
+                        "fieldname": "item_code",
+                        "fieldtype": "Link",
+                        "options": "Item",
+                        "width": 150
+                },
+                {
+                        "label": _("Amount"),
+                        "fieldname": "amount",
+                        "fieldtype": "Float",
+                        "width": 120
+                }
+        ]
+        return columns
 
-        def get_data(filters):
-                datasales =  frappe.db.sql("""
-                        SELECT
-                                `tabSales Order Item`.item_code,
-                                sum(`tabSales Order Item`.amount)
-                        FROM
-                                `tabSales Order Item`,`tabSales Order`
-                        WHERE
-                                `tabSales Order Item`.`parent`=`tabSales Order`.`name`
-        			AND `tabSales Order`.transaction_date BETWEEN %(from_date)s AND %(to_date)s
-                                {conditions}
-                        GROUP BY
-                                `tabSales Order Item`.item_code """.format(conditions=get_conditions(filters)), filters,
-        as_list=1)
+def get_data(filters):
+        datasales =  frappe.db.sql("""
+                SELECT
+                        `tabSales Order Item`.item_code,
+                        sum(`tabSales Order Item`.amount)
+                FROM
+                        `tabSales Order Item`,`tabSales Order`
+                WHERE
+                        `tabSales Order Item`.`parent`=`tabSales Order`.`name`
+			AND `tabSales Order`.transaction_date BETWEEN %(from_date)s AND %(to_date)s
+                        {conditions}
+                GROUP BY
+                        `tabSales Order Item`.item_code """.format(conditions=get_conditions(filters)), filters,
+as_list=1)
 
         return datasales
 
@@ -55,6 +49,5 @@ def get_conditions(filters) :
 
         if filters.get("item_code"):
                 conditions.append(" and `tabSales Order Item`.item_code=%(item_code)s")
-#        if filters.range == "Week":
-#                conditions.append(" and `tabSales Order`.transaction_date = %(from_date)s AND %(to_date)s")
+
         return " ".join(conditions) if conditions else ""
