17d08820c3b85b90947061e3b45a30e28e33c7fb
nishchay2698 <67817531+nishchay2698@users.noreply.github.com>
2021-02-26 16:05:47 +0530

    Update sa.py

--- scap1/scap1/report/sa/sa.py
+++ scap1/scap1/report/sa/sa.py
@@ -27,7 +27,7 @@ def get_columns():
         ]
         return columns
 
-def get_data():
+def get_data(filters):
         datasales =  frappe.db.sql("""
                 SELECT
                         `tabSales Order Item`.item_code,
