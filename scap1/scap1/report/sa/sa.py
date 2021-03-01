1877f339b6d25d46fea40688ad5d478f2a15515e
nishchay2698 <67817531+nishchay2698@users.noreply.github.com>
2021-03-01 14:10:37 +0530

    Update sa.py

--- scap1/scap1/report/sa/sa.py
+++ scap1/scap1/report/sa/sa.py
@@ -6,8 +6,8 @@ from six import iteritems
 from erpnext.accounts.utils import get_fiscal_year
 
 def execute(filters=None):
-        columns, data = get_columns(filters), get_data()
-
+        columns, data = get_columns(filters), get_data(filters)
+        
         return columns, data
 """
 def get_columns():
