8368a5615daa5d6296dc71351018044f6bb8c25c
nishchay2698 <67817531+nishchay2698@users.noreply.github.com>
2021-03-01 16:14:48 +0530

    Update sa.py

--- scap1/scap1/report/sa/sa.py
+++ scap1/scap1/report/sa/sa.py
@@ -145,7 +145,7 @@ def get_data(filters):
             return datasales
 
         if filters.get('range') == ('Quarterly'):
-            year = 2020
+            year = "2020"
             
             datasales1 =  frappe.db.sql("""
                     SELECT
@@ -165,7 +165,7 @@ def get_data(filters):
                 AND `tabSales Order`.transaction_date BETWEEN %(from_date)s AND %(to_date)s
                             {conditions}
                     GROUP BY
-                            `tabSales Order Item`.item_code """.format(conditions=get_conditions(filters)), year=year, filters, as_list=1)
+                            `tabSales Order Item`.item_code """.format(conditions=get_conditions(filters)), filters, as_list=1)
 
             return datasales1
 
