4287d6bfdf084ad1c4aacfc825372de1de3fa507
nishchay2698 <67817531+nishchay2698@users.noreply.github.com>
2021-03-28 13:00:50 +0530

    Update price_list_prince.py

--- scap1/scap1/report/price_list_prince/price_list_prince.py
+++ scap1/scap1/report/price_list_prince/price_list_prince.py
@@ -34,8 +34,8 @@ def get_data(filters):
         datasales =  frappe.db.sql("""
                 SELECT
                         `tabItem Price`.item_code,
-                        IF(price_list = "MRP",price_list_rate,NULL),
-                        IF(%(price_list_rate)s)
+                        (IF(price_list = "MRP",price_list_rate,NULL)),
+                        %(price_list_rate)s
                 FROM
                         `tabItem Price`
                         {conditions}""".format(conditions=get_conditions(filters)), filters,as_list=1)
